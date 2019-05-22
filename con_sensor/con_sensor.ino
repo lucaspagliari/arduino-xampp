cc3fb009ee6757d5cca73c77dc8dba4714d25dde
Lucas Pagliari <lucas.alex.pagliari@gmail.com>
2019-05-22 14:58:00 -0300

    codigo_sensor

--- con_sensor/con_sensor.ino
+++ con_sensor/con_sensor.ino
@@ -1,22 +1,33 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include <stdio.h>
+#include <string.h>
 
+
+#define MQ_analog A1
+#define MQ_dig 7
 
 byte mac[] = {
-    0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+    0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xAD
 };
 
+int valor_analog;
+int valor_dig;
+String url, valor;
+
 //Ip da placa
-IPAddress ip(192, 168, 15, 3);
-IPAddress gateway(192, 168, 15, 3);
+IPAddress ip(192, 168, 0, 32);
+IPAddress gateway(192, 168, 0, 32);
 
 // Ip que será conectado
-IPAddress server(192,168,1,15);
+IPAddress server(192,168,0,102);
 
 EthernetClient client;
 
 void setup() {
     Serial.begin(9600);
+    pinMode(MQ_analog, INPUT);
+    pinMode(MQ_dig, INPUT);
         while (!Serial) {
         ;
     }
@@ -33,19 +44,30 @@ void setup() {
 // possivelmente será possível passar os dados dos sensores
 void loop(){
     //Conexão
+    valor = valorSensor();
+    url = "GET /arduino.php?inf=" + valor;
+    Serial.println(url);
     if (client.connect(server, 80)) {
         Serial.println("Conectado!");
         
         // Faz a requisição http, definindo a var inf = ola
-        client.println("GET /arduino.php?inf=olaaa");
-
+        client.println(url);
+    
         // Não sei a necessidade desta parte
-        client.println("Host: 192.168.0.100");
-        client.println("Connection: close");
+        client.println("Host: 192.168.0.102");
+        //client.println("Connection: close");
         client.println();
     } 
     else {
         Serial.println("Falha ao conectar.");
     }
-    delay(4000);
+    delay(2000);
+}
+
+String valorSensor() {
+   valor_analog = analogRead(MQ_analog); 
+   valor_dig = digitalRead(MQ_dig);
+   delay(500);
+   return String(valor_analog);
+ 
 }
